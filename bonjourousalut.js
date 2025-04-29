f9d59484e852e9ad785946c44e2035907cf3179b
eloflo41 <elo.robert41@gmail.com>
2025-04-29 22:42:36 +0200

    boujourousalut

---
+++ bonjourousalut.js
@@ -0,0 +1,9 @@
+const choix = prompt("Tapez 1 ou 2.");
+
+if (choix === "1") {
+    console.log("Bonjour !");
+} else if (choix === "2") {
+    console.log("Salut !");
+} else {
+    console.log("Tu doit taper 1 ou 2 !");
+}
